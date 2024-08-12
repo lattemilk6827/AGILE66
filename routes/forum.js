87ff9cebd4e0eb08bc35b00d1c78694370ab58f5
kokei75 <sarahsophiagarcia755@gmail.com>
2024-08-12 09:09:55 +0800

    fixed db
    able to handle <br> in text

--- routes/forum.js
+++ routes/forum.js
@@ -171,11 +171,6 @@ function fetchForumPost(forumId, callback) {
             return;
         }
 
-        if (forum) {
-            // Replace newlines with <br> tags for proper HTML rendering
-            forum.forum_body = forum.forum_body.replace(/\n/g, '<br>');
-        }
-
         global.db.all(commentsQuery, [forumId], (err, comments) => {
             if (err) {
                 console.error('Error fetching comments:', err);
@@ -186,6 +181,7 @@ function fetchForumPost(forumId, callback) {
         });
     });
 }
+
 
 
 router.get('/forum/:id', requireAuth, (req, res) => {
